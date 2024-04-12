4b18897ba2de65fdb34e6876e1799957b52a00f9
Javier López de Ancos <javierlopezdeancos@gmail.com>
2024-04-12 21:29:12 +0200

    chore: update new folders to config files

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,11 @@ import type { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-  modulePathIgnorePatterns: ["<rootDir>/src/example-project/", "<rootDir>/build/"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/src/example-hello-world-app/",
+    "<rootDir>/src/example-hello-world-app-tests/",
+    "<rootDir>/build/",
+  ],
 };
 
 export default config;
