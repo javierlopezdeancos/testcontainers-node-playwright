cebe03bae7409ba58361db9c34d3d6a4bc709e5f
Javier López de Ancos <javierlopezdeancos@gmail.com>
2024-04-12 21:30:29 +0200

    feat: add pointed to example hello world container app tests

--- src/playwright-container.test.ts
+++ src/playwright-container.test.ts
@@ -1,225 +1,250 @@
 import path from "node:path";
 import { existsSync } from "node:fs";
-import { PlaywrightContainer, BROWSER } from "./playwright-container";
+import { Wait, Network, GenericContainer, StartedTestContainer, StartedNetwork } from "testcontainers";
+import { PlaywrightContainer, StartedPlaywrightContainer, BROWSER } from "./playwright-container";
 
 describe("PlaywrightContainer", () => {
-  jest.setTimeout(180_000);
+  const TEST_TIMEOUT = 260_000;
 
-  const PLAYWRIGHT_IMAGE = "mcr.microsoft.com/playwright:v1.42.1-jammy";
+  const PLAYWRIGHT_IMAGE = "mcr.microsoft.com/playwright:v1.43.0-jammy";
+  const HELLO_WORLD_APP_IMAGE = "javierland/example-hello-world-app:latest";
+
+  const EXTERNAL_HELLO_WORLD_APP_PORT_TO_BE_TESTED = "3000";
+  const EXTERNAL_HELLO_WORLD_APP_ALIAS_TO_BE_TESTED = "hello-world-app";
+  const EXTERNAL_HELLO_WORLD_APP_URL_TO_BE_TESTED = `http://${EXTERNAL_HELLO_WORLD_APP_ALIAS_TO_BE_TESTED}:${EXTERNAL_HELLO_WORLD_APP_PORT_TO_BE_TESTED}`;
+
   const EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY = path.resolve(__dirname, "..", "example-reports");
   const EXTERNAL_PLAYWRIGHT_SAVE_TRACES_DIRECTORY = path.resolve(__dirname, "..", "example-results");
-  const EXTERNAL_PLAYWRIGHT_PROJECT_DIRECTORY = path.resolve(__dirname, "example-project");
+  const EXTERNAL_HELLO_WORLD_APP_TESTS_DIRECTORY = path.resolve(__dirname, "example-hello-world-app-tests");
 
   const SUCCESSFUL_TEST_RUNNING_OUTPUT = "Running";
   const SUCCESSFUL_TEST_PASSED_OUTPUT = "passed";
   const SUCCESSFUL_TEST_EXIT_CODE = 0;
   const FAILED_TEST_EXIT_CODE = 1;
 
-  it(`should pass example tests with a dot build in reporter`, async () => {
-    const startedPlaywrightBuildInReporterContainer = await new PlaywrightContainer(
-      PLAYWRIGHT_IMAGE,
-      EXTERNAL_PLAYWRIGHT_PROJECT_DIRECTORY,
-    ).start();
+  const network: Network = new Network();
 
-    const { output, exitCode } = await startedPlaywrightBuildInReporterContainer.exec([
-      "npx",
-      "playwright",
-      "test",
-      "tests/example-success.spec.ts",
-      "--reporter=dot",
-    ]);
+  let startedNetwork: StartedNetwork;
+  let helloWorldAppStartedContainer: StartedTestContainer;
+  let startedPlaywrightContainer: StartedPlaywrightContainer;
 
-    await startedPlaywrightBuildInReporterContainer.stop();
+  beforeAll(async () => {
+    startedNetwork = await network.start();
 
-    expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
-    expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
-    expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
-  });
-
-  it(`should pass example tests with a line build in reporter`, async () => {
-    const startedPlaywrightBuildInReporterContainer = await new PlaywrightContainer(
-      PLAYWRIGHT_IMAGE,
-      EXTERNAL_PLAYWRIGHT_PROJECT_DIRECTORY,
-    ).start();
-
-    const { output, exitCode } = await startedPlaywrightBuildInReporterContainer.exec([
-      "npx",
-      "playwright",
-      "test",
-      "tests/example-success.spec.ts",
-      "--reporter=line",
-    ]);
-
-    await startedPlaywrightBuildInReporterContainer.stop();
-
-    expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
-    expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
-    expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
-  });
-
-  it(`should pass example tests creating an html reporter`, async () => {
-    const externalDestinationReporterPath = path.resolve(EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY, "index.html");
-
-    const startedPlaywrightContainer = await new PlaywrightContainer(
-      PLAYWRIGHT_IMAGE,
-      EXTERNAL_PLAYWRIGHT_PROJECT_DIRECTORY,
-    )
-      .withEnvironment({ PLAYWRIGHT_HTML_REPORT: "test-reports" })
+    helloWorldAppStartedContainer = await new GenericContainer(HELLO_WORLD_APP_IMAGE)
+      .withNetwork(startedNetwork)
+      .withNetworkAliases(EXTERNAL_HELLO_WORLD_APP_ALIAS_TO_BE_TESTED)
+      .withExposedPorts(parseInt(EXTERNAL_HELLO_WORLD_APP_PORT_TO_BE_TESTED, 10))
+      .withWaitStrategy(
+        Wait.forHttp("/health", parseInt(EXTERNAL_HELLO_WORLD_APP_PORT_TO_BE_TESTED, 10)).forStatusCodeMatching(
+          (statusCode: number): boolean => statusCode === 200,
+        ),
+      )
       .start();
 
-    const { output, exitCode } = await startedPlaywrightContainer.exec([
-      "npx",
-      "playwright",
-      "test",
-      "tests/example-success.spec.ts",
-      "--reporter=html",
-    ]);
+    await helloWorldAppStartedContainer.exec(["npx", "start"]);
 
-    await startedPlaywrightContainer.saveReporter("html", externalDestinationReporterPath);
-
-    expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
-    expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
-    expect(output).toContain("To open last HTML report run");
-    expect(output).toContain("npx playwright show-report");
-    expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
-    expect(existsSync(externalDestinationReporterPath)).toBe(true);
-
-    await startedPlaywrightContainer.stop();
-  });
-
-  it(`should pass example tests creating a json reporter`, async () => {
-    const externalDestinationReporterPath = path.resolve(EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY, "results.json");
-
-    const startedPlaywrightContainer = await new PlaywrightContainer(
+    startedPlaywrightContainer = await new PlaywrightContainer(
       PLAYWRIGHT_IMAGE,
-      EXTERNAL_PLAYWRIGHT_PROJECT_DIRECTORY,
+      EXTERNAL_HELLO_WORLD_APP_TESTS_DIRECTORY,
     )
-      .withEnvironment({ PLAYWRIGHT_JSON_OUTPUT_NAME: "results.json" })
+      .withNetwork(startedNetwork)
+      .withEnvironment({
+        APP_CONTAINER_URL_TO_GO_TO: EXTERNAL_HELLO_WORLD_APP_URL_TO_BE_TESTED,
+        PLAYWRIGHT_HTML_REPORT: "test-reports",
+        PLAYWRIGHT_JSON_OUTPUT_NAME: "results.json",
+        PLAYWRIGHT_JUNIT_OUTPUT_NAME: "results.xml",
+      })
       .start();
+  }, TEST_TIMEOUT);
 
-    const { output, exitCode } = await startedPlaywrightContainer.exec([
-      "npx",
-      "playwright",
-      "test",
-      "tests/example-success.spec.ts",
-      "--reporter=json",
-    ]);
+  it(
+    `should pass example tests with a dot build in reporter`,
+    async () => {
+      const { output, exitCode } = await startedPlaywrightContainer.exec([
+        "npx",
+        "playwright",
+        "test",
+        "tests/example-success.spec.ts",
+        "--reporter=dot",
+      ]);
 
-    await startedPlaywrightContainer.saveReporter("json", externalDestinationReporterPath);
-    await startedPlaywrightContainer.stop();
+      expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
+      expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
+      expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
+    },
+    TEST_TIMEOUT,
+  );
 
-    expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
-    expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
-    expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
-    expect(existsSync(externalDestinationReporterPath)).toBe(true);
-  });
+  it(
+    `should pass example tests with a line build in reporter`,
+    async () => {
+      const { output, exitCode } = await startedPlaywrightContainer.exec([
+        "npx",
+        "playwright",
+        "test",
+        "tests/example-success.spec.ts",
+        "--reporter=line",
+      ]);
 
-  it(`should pass example tests creating a bob reporter`, async () => {
-    const externalDestinationReporterPath = path.resolve(
-      EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY,
-      `report-${process.arch}.zip`,
-    );
+      expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
+      expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
+      expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
+    },
+    TEST_TIMEOUT,
+  );
 
-    const startedPlaywrightContainer = await new PlaywrightContainer(
-      PLAYWRIGHT_IMAGE,
-      EXTERNAL_PLAYWRIGHT_PROJECT_DIRECTORY,
-    ).start();
+  it(
+    `should pass example tests creating an html reporter`,
+    async () => {
+      const externalDestinationReporterPath = path.resolve(EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY, "index.html");
 
-    const { output, exitCode } = await startedPlaywrightContainer.exec([
-      "npx",
-      "playwright",
-      "test",
-      "tests/example-success.spec.ts",
-      "--reporter=blob",
-    ]);
+      const { output, exitCode } = await startedPlaywrightContainer.exec([
+        "npx",
+        "playwright",
+        "test",
+        "tests/example-success.spec.ts",
+        "--reporter=html",
+      ]);
 
-    await startedPlaywrightContainer.saveReporter("blob", externalDestinationReporterPath);
-    await startedPlaywrightContainer.stop();
+      await startedPlaywrightContainer.saveReporter("html", externalDestinationReporterPath);
 
-    expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
-    expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
-    expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
-    expect(existsSync(externalDestinationReporterPath)).toBe(true);
-  });
+      expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
+      expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
+      expect(output).toContain("To open last HTML report run");
+      expect(output).toContain("npx playwright show-report");
+      expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
+      expect(existsSync(externalDestinationReporterPath)).toBe(true);
+    },
+    TEST_TIMEOUT,
+  );
 
-  it(`should pass example tests creating a junit reporter`, async () => {
-    const externalDestinationReporterPath = path.resolve(EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY, `results.xml`);
+  it(
+    `should pass example tests creating a json reporter`,
+    async () => {
+      const externalDestinationReporterPath = path.resolve(EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY, "results.json");
 
-    const startedPlaywrightContainer = await new PlaywrightContainer(
-      PLAYWRIGHT_IMAGE,
-      EXTERNAL_PLAYWRIGHT_PROJECT_DIRECTORY,
-    )
-      .withEnvironment({ PLAYWRIGHT_JUNIT_OUTPUT_NAME: "results.xml" })
-      .start();
+      const { output, exitCode } = await startedPlaywrightContainer.exec([
+        "npx",
+        "playwright",
+        "test",
+        "tests/example-success.spec.ts",
+        "--reporter=json",
+      ]);
 
-    const { output, exitCode } = await startedPlaywrightContainer.exec([
-      "npx",
-      "playwright",
-      "test",
-      "tests/example-success.spec.ts",
-      "--reporter=junit",
-    ]);
+      await startedPlaywrightContainer.saveReporter("json", externalDestinationReporterPath);
 
-    await startedPlaywrightContainer.saveReporter("junit", externalDestinationReporterPath);
-    await startedPlaywrightContainer.stop();
+      expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
+      expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
+      expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
+      expect(existsSync(externalDestinationReporterPath)).toBe(true);
+    },
+    TEST_TIMEOUT,
+  );
 
-    expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
-    expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
-    expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
-    expect(existsSync(externalDestinationReporterPath)).toBe(true);
-  });
-
-  it("should fail example test creating a trace viewer by browser on first retry", async () => {
-    const startedPlaywrightContainer = await new PlaywrightContainer(
-      PLAYWRIGHT_IMAGE,
-      EXTERNAL_PLAYWRIGHT_PROJECT_DIRECTORY,
-    ).start();
-
-    const { exitCode } = await startedPlaywrightContainer.exec([
-      "npx",
-      "playwright",
-      "test",
-      "tests/example-fail.spec.ts",
-      "--trace",
-      "on",
-    ]);
-
-    const TEST_FAILED_NAME_GET_STARTED_LINK_IN_KEBAB_CASE = "example-fail-get-started-link";
-    const TEST_FAILED_NAME_HAS_TITLE_IN_KEBAB_CASE = "example-fail-has-title";
-
-    const browsers = Object.values(BROWSER);
-
-    await startedPlaywrightContainer.saveTraceViewer(
-      browsers,
-      TEST_FAILED_NAME_GET_STARTED_LINK_IN_KEBAB_CASE,
-      EXTERNAL_PLAYWRIGHT_SAVE_TRACES_DIRECTORY,
-    );
-
-    await startedPlaywrightContainer.saveTraceViewer(
-      browsers,
-      TEST_FAILED_NAME_HAS_TITLE_IN_KEBAB_CASE,
-      EXTERNAL_PLAYWRIGHT_SAVE_TRACES_DIRECTORY,
-    );
-
-    await startedPlaywrightContainer.stop();
-
-    expect(exitCode).toBe(FAILED_TEST_EXIT_CODE);
-
-    for (const browser of browsers) {
-      const externalTraceViewerDestinationToGetStartedLinkTestFailed = path.resolve(
-        EXTERNAL_PLAYWRIGHT_SAVE_TRACES_DIRECTORY,
-        `example-fail-get-started-link-${browser}`,
-        "trace.zip",
+  it(
+    `should pass example tests creating a bob reporter`,
+    async () => {
+      const externalDestinationReporterPath = path.resolve(
+        EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY,
+        `report-${process.arch}.zip`,
       );
 
-      const externalTraceViewerDestinationToHasTitleTestFailed = path.resolve(
+      const { output, exitCode } = await startedPlaywrightContainer.exec([
+        "npx",
+        "playwright",
+        "test",
+        "tests/example-success.spec.ts",
+        "--reporter=blob",
+      ]);
+
+      await startedPlaywrightContainer.saveReporter("blob", externalDestinationReporterPath);
+
+      expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
+      expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
+      expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
+      expect(existsSync(externalDestinationReporterPath)).toBe(true);
+    },
+    TEST_TIMEOUT,
+  );
+
+  it(
+    `should pass example tests creating a junit reporter`,
+    async () => {
+      const externalDestinationReporterPath = path.resolve(EXTERNAL_PLAYWRIGHT_SAVE_REPORTS_DIRECTORY, `results.xml`);
+
+      const { output, exitCode } = await startedPlaywrightContainer.exec([
+        "npx",
+        "playwright",
+        "test",
+        "tests/example-success.spec.ts",
+        "--reporter=junit",
+      ]);
+
+      await startedPlaywrightContainer.saveReporter("junit", externalDestinationReporterPath);
+
+      expect(output).toContain(SUCCESSFUL_TEST_RUNNING_OUTPUT);
+      expect(output).toContain(SUCCESSFUL_TEST_PASSED_OUTPUT);
+      expect(exitCode).toBe(SUCCESSFUL_TEST_EXIT_CODE);
+      expect(existsSync(externalDestinationReporterPath)).toBe(true);
+    },
+    TEST_TIMEOUT,
+  );
+
+  it(
+    "should fail example test creating a trace viewer by browser on first retry",
+    async () => {
+      const { exitCode } = await startedPlaywrightContainer.exec([
+        "npx",
+        "playwright",
+        "test",
+        "tests/example-fail.spec.ts",
+        "--trace",
+        "on",
+      ]);
+
+      const TEST_FAILED_NAME_GET_STARTED_LINK_IN_KEBAB_CASE = "example-fail-get-started-link";
+      const TEST_FAILED_NAME_HAS_TITLE_IN_KEBAB_CASE = "example-fail-has-title";
+
+      const browsers = Object.values(BROWSER);
+
+      await startedPlaywrightContainer.saveTraceViewer(
+        browsers,
+        TEST_FAILED_NAME_GET_STARTED_LINK_IN_KEBAB_CASE,
         EXTERNAL_PLAYWRIGHT_SAVE_TRACES_DIRECTORY,
-        `example-fail-get-started-link-${browser}`,
-        "trace.zip",
       );
 
-      expect(existsSync(externalTraceViewerDestinationToGetStartedLinkTestFailed)).toBe(true);
-      expect(existsSync(externalTraceViewerDestinationToHasTitleTestFailed)).toBe(true);
-    }
-  });
+      await startedPlaywrightContainer.saveTraceViewer(
+        browsers,
+        TEST_FAILED_NAME_HAS_TITLE_IN_KEBAB_CASE,
+        EXTERNAL_PLAYWRIGHT_SAVE_TRACES_DIRECTORY,
+      );
+
+      expect(exitCode).toBe(FAILED_TEST_EXIT_CODE);
+
+      for (const browser of browsers) {
+        const externalTraceViewerDestinationToGetStartedLinkTestFailed = path.resolve(
+          EXTERNAL_PLAYWRIGHT_SAVE_TRACES_DIRECTORY,
+          `example-fail-get-started-link-${browser}`,
+          "trace.zip",
+        );
+
+        const externalTraceViewerDestinationToHasTitleTestFailed = path.resolve(
+          EXTERNAL_PLAYWRIGHT_SAVE_TRACES_DIRECTORY,
+          `example-fail-get-started-link-${browser}`,
+          "trace.zip",
+        );
+
+        expect(existsSync(externalTraceViewerDestinationToGetStartedLinkTestFailed)).toBe(true);
+        expect(existsSync(externalTraceViewerDestinationToHasTitleTestFailed)).toBe(true);
+      }
+    },
+    TEST_TIMEOUT,
+  );
+
+  afterAll(async () => {
+    await startedPlaywrightContainer.stop();
+    await helloWorldAppStartedContainer.stop();
+    await startedNetwork.stop();
+  }, TEST_TIMEOUT);
 });
