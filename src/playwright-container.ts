ebc4ea9a4c17b003429b24c50d592ffb8792e6b9
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2024-07-10 20:42:14 +0200

    chore: update to playwright v1.45.1-jammy

--- src/playwright-container.ts
+++ src/playwright-container.ts
@@ -14,7 +14,7 @@ import {
   log,
 } from "testcontainers";
 
-export const DEFAULT_PLAYWRIGHT_CONTAINER_IMAGE = "mcr.microsoft.com/playwright:v1.44.0-jammy";
+export const DEFAULT_PLAYWRIGHT_CONTAINER_IMAGE = "mcr.microsoft.com/playwright:v1.45.1-jammy";
 
 export const CONTAINER_WORKING_DIRECTORY = "/playwright";
 
